c1278a2698e00dbdddc08c6d4f14c9c8aa9a6e53
sivaaks <senthilsivaaks@gmail.com>
2021-10-05 14:43:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from './components/Login';
+import Register from './components/Register';
+import ListUsers from './components/ListUsers';
+import Dashboard from './components/Dashboard';
+import Leads from './components/Leads';
+import PrivateRoute from './components/PrivateRoute';
+import ServiceRequests from './components/ServiceRequests';
+import Alert from './components/Alerts';
+
+export default function App(){
+  return(
+    <BrowserRouter>
+      <Switch>
+          <Route exact path='/login' component={Login}></Route>
+          <Route exact path='/register' component={Register}></Route>
+          <Route exact path='/forgot-password' component={Login}></Route>
+          <Route exact path='/alert' component={Alert}></Route>
+          <PrivateRoute exact path='/dashboard' component={Dashboard}></PrivateRoute>
+          <PrivateRoute exact path='/leads' component={Leads}></PrivateRoute>
+          <PrivateRoute exact path='/users' component={ListUsers}></PrivateRoute>
+          <PrivateRoute exact path='/service-requests' component={ServiceRequests}></PrivateRoute>
+          <Route path='*' render={()=><p>404 NOT FOUND</p>}></Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
-
-export default App;
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+
+import Header from './Header';
+
+
+export default function Dashboard(){
+
+    return (
+        <>
+        <Container>
+            <Header/>
+            <h3>Dashboard</h3>
+        </Container>
+
+        </>
+    )
+
+}
